f61fa7ee0d66cfa7423fd18184e2633129913455
shubidumdu <skaty4870@naver.com>
2023-08-31 17:57:49 +0900

    Add comment for using webgpu

--- main.ts
+++ main.ts
@@ -8,18 +8,21 @@ const parse = (str: string) =>
     .join(' ');
 
 document.body.innerHTML = `
-  <ul class="page-list">
-    ${pageNames
-      .map(
-        (page) => `
-      <li>
-        <a href="./pages/${page}/">
-          <img src="./thumbnails/${page}.png" alt="${parse(page)}">
-          <span>${parse(page)}</span>
-        </a>
-      </li>
-    `,
-      )
-      .join('')}
-  </ul>
+  <main>
+    <p>Some projects use the WebGPU API, so they may not display properly depending on your device and browser environment.</p>
+    <ul class="page-list">
+      ${pageNames
+        .map(
+          (page) => `
+        <li>
+          <a href="./pages/${page}/">
+            <img src="./thumbnails/${page}.png" alt="${parse(page)}">
+            <span>${parse(page)}</span>
+          </a>
+        </li>
+      `,
+        )
+        .join('')}
+    </ul>
+  </main>
 `;
