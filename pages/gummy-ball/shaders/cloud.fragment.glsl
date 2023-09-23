46bab88fbc4d68809d15f9a09709445b25721e0c
shubidumdu <skaty4870@naver.com>
2023-09-23 16:00:02 +0900

    Change cloud color

---
+++ pages/gummy-ball/shaders/cloud.fragment.glsl
@@ -0,0 +1,29 @@
+#version 300 es
+precision highp float;
+
+in vec3 vPosition;
+in vec3 vNormal;
+
+uniform mat4 world;
+uniform mat4 worldViewProjection;
+uniform vec3 pointerRay;
+uniform float clickedTime;
+
+out vec4 outColor;
+
+const vec3 lightPosition=vec3(0.,1.,0.);
+const vec3 lightPosition2=vec3(0,-1.,0.);
+
+const float rayThreshold=.99;
+
+void main(){
+  vec3 n=normalize(vNormal);
+  vec3 v=normalize(vPosition);
+  vec3 l=normalize(lightPosition);
+  vec3 r=normalize(pointerRay);
+  float light=dot(n,l);
+  float light2=dot(n,normalize(lightPosition2));
+  outColor=vec4(vec3(0.125, .196, .4078),1.);
+  outColor+=vec4(mix(vec3(.6,.6,.6),vec3(1.),light),1.);
+  outColor.rgb+=vec3(.1,.1,.2)*light2;
+}
