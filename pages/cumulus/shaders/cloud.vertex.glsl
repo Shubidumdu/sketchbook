80208b228461b85f10349aae5d82237f3e6c5c34
shubidumdu <skaty4870@naver.com>
2023-10-04 15:46:52 +0900

    Adjust fBM in cumulus

--- pages/cumulus/shaders/cloud.vertex.glsl
+++ pages/cumulus/shaders/cloud.vertex.glsl
@@ -19,30 +19,40 @@ float random(in vec3 stu){
     
 float noise(in vec3 stu){
   vec3 i=floor(stu);
-  vec3 f=fract(stu);
+  vec3 fr=fract(stu);
   
   float a=random(i);
-  float b=random(i+vec3(1.,1., 0.));
-  float c=random(i+vec3(0.,1., 1.));
-  float d=random(i+vec3(1.,1., 1.));
+  float b=random(i+vec3(1.,0., 0.));
+  float c=random(i+vec3(0.,1., 0.));
+  float d=random(i+vec3(1.,1., 0.));
+  float e=random(i+vec3(0.,0., 1.));
+  float f=random(i+vec3(1.,0., 1.));
+  float g=random(i+vec3(0.,1., 1.));
+  float h=random(i+vec3(1.,1., 1.));
   
-  vec3 u=f*f*(2.8-2.*f);
-  
-  return mix(a,b,u.x)+
-  (c-a)*u.y*(1.-u.x)+
-  (d-b)*u.x*u.y;
+  vec3 u=fr*fr*(3.-2.*fr);
+
+  float x1 = mix(a, b, u.x);
+  float x2 = mix(c, d, u.x);
+  float x3 = mix(e, f, u.x);
+  float x4 = mix(g, h, u.x);
+
+  float y1 = mix(x1, x2, u.y);
+  float y2 = mix(x3, x4, u.y);
+
+  return mix(y1, y2, u.z);
 }
 
 #define OCTAVES 8
 float fbm (in vec3 stu) {
-  float value = 0.0;
+  float value = 0.;
   float amplitude = .8;
-  float frequency = 0.;
+  float frequency = 1.1;
 
   for (int i = 0; i < OCTAVES; i++) {
-      value += amplitude * noise(stu);
-      stu *= 1.5;
-      amplitude *= .6;
+      value += amplitude * noise(frequency * stu);
+      stu *= 1.2;
+      amplitude *= .7;
   }
   return value;
 }
@@ -51,7 +61,7 @@ void main(){
   vPosition=mat3(world)*position;
   vNormal=mat3(world)*normal;
   gl_Position=worldViewProjection*vec4(
-    position + normal * vec3(fbm(normal + time * 0.0003)), 
+    position + normal * vec3(fbm(normal + time * 0.001)), 
     1.
   );
 }
