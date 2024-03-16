133ad3c05c31aac8aae3d550bc40b6c5c4ba4e36
shubidumdu <skaty4870@naver.com>
2024-03-17 00:24:07 +0900

    Show multiple meshes

--- pages/visualizer/main.ts
+++ pages/visualizer/main.ts
@@ -4,6 +4,8 @@ import {
   Color4,
   ComputeShader,
   Constants,
+  DataBuffer,
+  Engine,
   MeshBuilder,
   Scene,
   ShaderMaterial,
@@ -16,11 +18,11 @@ import {
 import computeShaderSource from './shaders/compute.wgsl?raw';
 import particleFragmentShaderSource from './shaders/fragment.glsl?raw';
 import particleVertexShaderSource from './shaders/vertex.glsl?raw';
-import { Inspector } from '@babylonjs/inspector';
 
 const canvas = document.getElementById('babylon') as HTMLCanvasElement;
 const engine = new WebGPUEngine(canvas);
 await engine.initAsync();
+// engine.compatibilityMode = false;
 const scene = new Scene(engine);
 scene.clearColor = Color4.FromHexString('#212E33');
 
@@ -36,98 +38,128 @@ camera.attachControl(canvas, true);
 
 const PARTICLE_NUMS = 100;
 
+const mesh = MeshBuilder.CreatePolyhedron('oct', { type: 3, size: 1 }, scene);
+mesh.forcedInstanceCount = PARTICLE_NUMS;
+
+const particleMeshMaterial = new ShaderMaterial(
+  'particle',
+  scene,
+  {
+    vertexSource: particleVertexShaderSource,
+    fragmentSource: particleFragmentShaderSource,
+  },
+  {
+    attributes: ['position', 'v_position', 'p_position', 'velocity', 'normal'],
+    uniforms: ['time', 'world', 'worldViewProjection'],
+  },
+);
+
+// particleMeshMaterial.wireframe = true;
+
+mesh.material = particleMeshMaterial;
+mesh.material.wireframe = true;
+
+const initialVertexPositions = mesh.getVerticesData(VertexBuffer.PositionKind)!;
+
+const vertexPositionBuffer = (() => {
+  // const newBuffer = new Float32Array(initialVertexPositions.length);
+
+  // [...Array(PARTICLE_NUMS)].forEach((_, index) =>
+  //   newBuffer.set(
+  //     initialVertexPositions,
+  //     index * initialVertexPositions.length,
+  //   ),
+  // );
+
+  return new VertexBuffer(
+    engine,
+    initialVertexPositions,
+    'v_position',
+    false,
+    false,
+    3,
+    false,
+  );
+})();
+
+// mesh.setIndices(indices!);
+mesh.setVerticesBuffer(vertexPositionBuffer);
+
+// const realVertexBuffer = new VertexBuffer(engine);
+
 // Compute
+
 const initialParticles = new Float32Array(
   [...new Array(PARTICLE_NUMS)]
     .map(() => [
       Math.random() * 2 - 1, // position
       Math.random() * 2 - 1,
       Math.random() * 2 - 1,
-      // Math.random() * 2 - 1, // p_direction
-      // Math.random() * 2 - 1,
-      // Math.random() * 2 - 1,
-      // Math.random() * 2 - 1, // velocity
-      // Math.random() * 2 - 1,
-      // Math.random() * 2 - 1,
+      Math.random() * 2 - 1, // p_direction
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1, // velocity
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
     ])
     .flat(),
 );
 
-const uniforms = new UniformBuffer(engine, undefined, undefined, 'uniforms');
-uniforms.addUniform('deltaTime', 1);
-uniforms.updateFloat('deltaTime', 1);
-uniforms.addUniform('particleCount', PARTICLE_NUMS);
-uniforms.updateUInt('particleCount', PARTICLE_NUMS);
-uniforms.update();
+// const uniforms = new UniformBuffer(engine, undefined, undefined, 'uniforms');
+// uniforms.addUniform('deltaTime', 1);
+// uniforms.updateFloat('deltaTime', 1);
+// uniforms.addUniform('particleCount', PARTICLE_NUMS);
+// uniforms.updateUInt('particleCount', PARTICLE_NUMS);
+// uniforms.update();
 
 const particleBuffer = new StorageBuffer(
   engine,
   initialParticles.byteLength,
   Constants.BUFFER_CREATIONFLAG_STORAGE |
-    // Constants.BUFFER_CREATIONFLAG_VERTEX |
+    Constants.BUFFER_CREATIONFLAG_VERTEX |
     Constants.BUFFER_CREATIONFLAG_READ |
     Constants.BUFFER_CREATIONFLAG_WRITE,
 );
 
 particleBuffer.update(initialParticles);
 
-const computeShader = new ComputeShader(
-  'computeShader',
+const positionBuffer = new VertexBuffer(
   engine,
-  {
-    computeSource: computeShaderSource,
-  },
-  {
-    bindingsMapping: {
-      uniforms: { group: 0, binding: 0 },
-      particles: { group: 0, binding: 1 },
-    },
-  },
+  particleBuffer.getBuffer(),
+  'p_position',
+  false,
+  false,
+  9,
+  true,
+  0,
+  3,
 );
 
-computeShader.setUniformBuffer('uniforms', uniforms);
-computeShader.setStorageBuffer('particles', particleBuffer);
+mesh.setVerticesBuffer(positionBuffer, false);
 
-const mesh = MeshBuilder.CreatePolyhedron('oct', {
-  type: 3,
-  size: 1,
-  updatable: true,
-});
+// const computeShader = new ComputeShader(
+//   'computeShader',
+//   engine,
+//   {
+//     computeSource: computeShaderSource,
+//   },
+//   {
+//     bindingsMapping: {
+//       uniforms: { group: 0, binding: 0 },
+//       particles: { group: 0, binding: 1 },
+//     },
+//   },
+// );
 
-[...Array(PARTICLE_NUMS - 1)].forEach((_, index) => {
-  const instanced = mesh.createInstance(`mesh${index}`);
-  instanced.setAbsolutePosition(
-    new Vector3(
-      initialParticles[index * 3 + 0],
-      initialParticles[index * 3 + 1],
-      initialParticles[index * 3 + 2],
-    ),
-  );
-});
+// computeShader.setUniformBuffer('uniforms', uniforms);
+// computeShader.setStorageBuffer('particles', particleBuffer);
 
-// mesh.material = particleMeshMaterial;
-
-// particleBuffer.getBuffer()
-
-// mesh.thinInstanceSetBuffer("a", )
-// mesh.createInstance('i0');
-// mesh.createInstance('i0');
-
-// console.log(mesh.getVertexBuffer('position')?.getFloatData());
-
-// mesh.setVerticesBuffer(vertexPositionBuffer);
-// mesh.setVerticesBuffer(vertexDirectionBuffer);
-// mesh.setVerticesBuffer(vertexVelocityBuffer);
-
-// const positionBuffer = mesh.getVertexBuffer(VertexBuffer.PositionKind);
-// console.log(mesh.getVertexBuffer('p_direction'));
+// // Vertex
 
 let time = 0;
 
 engine.runRenderLoop(() => {
-  computeShader.dispatch(Math.ceil(PARTICLE_NUMS / 64));
   time += scene.deltaTime;
-  // particleMeshMaterial.setFloat('time', time);
   scene.render();
   engine.resize();
 });
--- pages/visualizer/shaders/vertex.glsl
+++ pages/visualizer/shaders/vertex.glsl
@@ -1,8 +1,8 @@
 #version 300 es
 precision highp float;
 
+in vec3 v_position;
 in vec3 p_position;
-in vec3 p_direction;
 in vec3 velocity;
 in vec3 normal;
 
@@ -14,7 +14,7 @@ out vec3 vPosition;
 out vec3 vNormal;
 
 void main() {
-  vPosition = mat3(world)*p_position;
+  vPosition = mat3(world)*v_position;
   vNormal = mat3(world)*normal;
-  gl_Position = worldViewProjection * vec4(p_position, 1.);
+  gl_Position = worldViewProjection * vec4(v_position - p_position * 4., 1.);
 }
